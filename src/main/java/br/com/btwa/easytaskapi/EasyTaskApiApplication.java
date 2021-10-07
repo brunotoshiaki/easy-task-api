555c576153dbf4e94610ee0c0c602e30feb0cadb
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-10-07 20:33:24 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/btwa/easytaskapi/EasyTaskApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.btwa.easytaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/btwa/easytaskapi/EasyTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.btwa.easytaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
