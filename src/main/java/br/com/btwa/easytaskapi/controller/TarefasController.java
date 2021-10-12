5dbdc7c40ab521a4d3037f2b457abd4d3a9713d2
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-10-12 12:42:44 -0300

    Get por descricao e update

--- src/main/java/br/com/btwa/easytaskapi/controller/TarefasController.java
+++ src/main/java/br/com/btwa/easytaskapi/controller/TarefasController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,22 @@ public class TarefasController {
     @GetMapping("/tarefas")
     public List<Tarefa> getAll() {
         return tarefasRepository.findAll();
+    }
+
+    @GetMapping("/tarefas/{id}")
+    public Tarefa findById(@PathVariable String id) {
+        return tarefasRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("tarefas/descricao/{descricao}")
+    public List<Tarefa> findByDescricao(@PathVariable String descricao) {
+        return tarefasRepository.findByDescricaoLikeIgnoreCase(descricao);
+    }
+
+    @PutMapping("tarefas/{id}")
+    public Tarefa update(@PathVariable String id, @RequestBody Tarefa tarefa) {
+        tarefa.setId(id);
+        return tarefasRepository.save(tarefa);
     }
 
 }
