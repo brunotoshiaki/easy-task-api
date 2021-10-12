9c118a4f6ef41dc8a0c274ef525d4bf732e87f03
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-10-12 12:00:58 -0300

    Criando o findall

--- src/main/java/br/com/btwa/easytaskapi/controller/TarefasController.java
+++ src/main/java/br/com/btwa/easytaskapi/controller/TarefasController.java
@@ -1,6 +1,9 @@
 package br.com.btwa.easytaskapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,11 @@ public class TarefasController {
     @PostMapping("/tarefas")
     public Tarefa create(@RequestBody Tarefa tarefa) {
         return tarefasRepository.save(tarefa);
+    }
+
+    @GetMapping("/tarefas")
+    public List<Tarefa> getAll() {
+        return tarefasRepository.findAll();
     }
 
 }
